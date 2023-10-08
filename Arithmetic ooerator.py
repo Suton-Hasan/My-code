ccbb9bdca0cca62f3d8fad642be1193cd9c98c4f
Md Abu Bokar Siddike <126772251+Suton-Hasan@users.noreply.github.com>
2023-10-08 19:33:09 +0600

    Add files via upload

---
+++ Arithmetic ooerator.py
@@ -0,0 +1,10 @@
+A=int(input())
+B=int(input())
+if A==B:
+    print('Right')
+elif A>B:
+    print('Suton')
+elif A<B:
+    print('Disha')
+else:
+    print('')
---
+++ main.py
@@ -0,0 +1,6 @@
+A=10
+B=50
+if A>B:
+    print("Suton")
+else:
+    print('Disha')
