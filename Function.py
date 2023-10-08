4e078444d816fc40608b8b5626a9452055a33b4c
Md Abu Bokar Siddike <126772251+Suton-Hasan@users.noreply.github.com>
2023-10-08 19:52:37 +0600

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,17 @@
+
+def Suton(A,B):
+    sum=A+B
+    print(sum)
+def Disha(A,B):
+    sub=A-B
+    print(sub)
+def Nusu(A,B):
+    d=A/B
+    print(d)
+def Sumu(A,B):
+    m=A*B
+    print(m)
+Suton(10,6)
+Sumu(2,7)
+Disha(20,7)
+Nusu(6,2)
